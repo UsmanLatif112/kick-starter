025cb3b0b086815b5835e7e1f8f5ff3d2916fd6d
Usman0026 <144221769+Usman0026@users.noreply.github.com>
2024-06-13 00:04:43 +0500

    finalize

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from pages.Imports import *
 from pages.profile_page import main_code
 
 def main():
-
-    code = main_code()
+    for i in range(10000):
+        code = main_code()
       
 if __name__ == '__main__':
     main()
