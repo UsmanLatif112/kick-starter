7e501d6647434cf9048d9f1861b44483a51f787e
Usman0026 <144221769+Usman0026@users.noreply.github.com>
2024-06-18 13:20:54 +0500

    finalize

--- pages/profile_page.py
+++ pages/profile_page.py
@@ -28,11 +28,7 @@ def make_csv(filename: str, data, new=True):
     with open(filename, mode, newline="", encoding='utf-8') as f:
         f.writelines(data)
 
-<<<<<<< HEAD
 def main_code_1():
-=======
-def main_code():
->>>>>>> 025cb3b0b086815b5835e7e1f8f5ff3d2916fd6d
     try:
         driver = get_undetected_chrome_browser('christoph')
         time.sleep(1)
@@ -40,7 +36,6 @@ def main_code():
         time.sleep(1)
         driver.maximize_window()
         time.sleep(1)
-<<<<<<< HEAD
         for url in urls:
             try:
                 driver.get(url)
@@ -95,49 +90,6 @@ def main_code():
                     print(e)
             except Exception as e:
                 print(e)
-=======
-        driver.get("https://www.kickstarter.com/discover")
-        time.sleep(1)        
-        try:
-            side_modall = HomePage(driver)
-            side_modall.wait(ProfileResources.side_modal)
-            time.sleep(2)
-            side_modal_crosss = HomePage(driver)
-            side_modal_crosss.click_btn(ProfileResources.side_modal_cross)
-        except:
-            pass
-        project_card_xpath = ProfileResources.project_url
-        load_more_button_xpath = ProfileResources.load_more_btn
-        try:
-            # Main scraping loop
-            while True:
-                # Find all project cards
-                project_cards = driver.find_elements(By.XPATH, project_card_xpath)
-                for card in project_cards:
-                    project_url = card.get_attribute('href')
-                    if not is_url_in_csv(project_url, csv_file):
-                        print(project_url)
-                        save_url_to_csv(project_url, csv_file)
-                    
-                # Try to click the 'Load More' button
-                try:
-                    time.sleep(2)
-                    load_more_button = driver.find_element(By.XPATH, load_more_button_xpath)
-                    time.sleep(5)
-                    ActionChains(driver).move_to_element(load_more_button).perform()
-                    time.sleep(5)
-                    load_more_button.click()
-                    time.sleep(30)
-                except NoSuchElementException:
-                    crruent_url = driver.current_url
-                    print("No more 'Load More' button found or end of the list reached.")
-                    print(driver.current_url)
-                    break
-        except Exception as e:
-            print(e)
-        # Close the browser
-        driver.quit()
->>>>>>> 025cb3b0b086815b5835e7e1f8f5ff3d2916fd6d
     except Exception as e:
         print(e)
     # Close the browser
